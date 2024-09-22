16d85a93819236d940a9221e16ae43eee7770fa2
Nitesh <159615163+mrzitesh@users.noreply.github.com>
2024-09-22 20:37:42 +0530

    Update Second_Admission_Grant.java

--- Java/Second_Admission_Grant.java
+++ Java/Second_Admission_Grant.java
@@ -1,7 +1,10 @@
 /*
 Q2. Admission to a professional course is subject to the following conditions: 
- (a) marks in Mathematics >= 60 (b) marks in Physics >=50 (c) marks in Chemistry 
->=40 (d) Total in all 3 subjects >=200 (Or) Total in Maths & Physics>=150
+ (a) marks in Mathematics >= 60 (b) marks in Physics >=50 
+ (c) marks in Chemistry >=40    (d) Total in all 3 subjects >=200 (Or) Total in Maths & Physics>=150
+
+Given the marks in the 3 subjects of n (user input) students, write a program to process the 
+applications to list the eligible candidates.
 */
 
 import java.util.Scanner;
